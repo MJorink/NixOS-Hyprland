07006c2cf70bf14bfe79ad2cd8b1efcf1230d808
Jorink <maxjorink@gmail.com>
2025-06-10 21:40:44 +0200

    Update config.nix

--- hosts/default/config.nix
+++ hosts/default/config.nix
@@ -1,19 +1,20 @@
-# 💫 https://github.com/JaKooLit 💫 #
-# Main default config
+{ config, pkgs, host, username, options, lib, inputs, system, ... }:
 
+let
+  gitUsername = "example";
+  gitEmail = "example@example.com";
+  keyboardLayout = "us";
+  browser = "zen-browser";
+  terminal = "kitty";
+  clock24h = true;
+  extraMonitorSettings = "";
+in
 
-# NOTE!!! : Packages and Fonts are configured in packages-&-fonts.nix
+{
 
-
-{ config, pkgs, host, username, options, lib, inputs, system, ...}: let
   
-  inherit (import ./variables.nix) keyboardLayout;
-    
-  in {
   imports = [
     ./hardware.nix
-    ./users.nix
-    ./packages-fonts.nix
     ../../modules/amd-drivers.nix
     ../../modules/nvidia-drivers.nix
     ../../modules/nvidia-prime-drivers.nix
@@ -22,66 +23,25 @@
     ../../modules/local-hardware-clock.nix
   ];
 
-  # BOOT related stuff
   boot = {
-    kernelPackages = pkgs.linuxPackages_zen; # zen Kernel
-    #kernelPackages = pkgs.linuxPackages_latest; # Kernel 
-
+    kernelPackages = pkgs.linuxPackages_zen;
     kernelParams = [
       "systemd.mask=systemd-vconsole-setup.service"
-      "systemd.mask=dev-tpmrm0.device" #this is to mask that stupid 1.5 mins systemd bug
-      "nowatchdog" 
-      "modprobe.blacklist=sp5100_tco" #watchdog for AMD
-      "modprobe.blacklist=iTCO_wdt" #watchdog for Intel
- 	  ];
+      "systemd.mask=dev-tpmrm0.device"
+      "nowatchdog"
+      "modprobe.blacklist=sp5100_tco"
+      "modprobe.blacklist=iTCO_wdt"
+    ];
 
-    # This is for OBS Virtual Cam Support
-    #kernelModules = [ "v4l2loopback" ];
-    #  extraModulePackages = [ config.boot.kernelPackages.v4l2loopback ];
-    
-    initrd = { 
-      availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usb_storage" "usbhid" "sd_mod" ];
-      kernelModules = [ ];
-    };
+    initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usb_storage" "usbhid" "sd_mod" ];
 
-    # Needed For Some Steam Games
-    #kernel.sysctl = {
-    #  "vm.max_map_count" = 2147483642;
-    #};
-
-    ## BOOT LOADERS: NOTE USE ONLY 1. either systemd or grub  
-    # Bootloader SystemD
     loader.systemd-boot.enable = true;
-  
-    loader.efi = {
-	    #efiSysMountPoint = "/efi"; #this is if you have separate /efi partition
-	    canTouchEfiVariables = true;
-  	  };
+    loader.efi.canTouchEfiVariables = true;
+    loader.timeout = 5;
 
-    loader.timeout = 5;    
-  			
-    # Bootloader GRUB
-    #loader.grub = {
-	    #enable = true;
-	    #  devices = [ "nodev" ];
-	    #  efiSupport = true;
-      #  gfxmodeBios = "auto";
-	    #  memtest86.enable = true;
-	    #  extraGrubInstallArgs = [ "--bootloader-id=${host}" ];
-	    #  configurationName = "${host}";
-  	  #	 };
+    tmp.useTmpfs = false;
+    tmp.tmpfsSize = "30%";
 
-    # Bootloader GRUB theme, configure below
-
-    ## -end of BOOTLOADERS----- ##
-  
-    # Make /tmp a tmpfs
-    tmp = {
-      useTmpfs = false;
-      tmpfsSize = "30%";
-      };
-    
-    # Appimage Support
     binfmt.registrations.appimage = {
       wrapInterpreterInShell = false;
       interpreter = "${pkgs.appimage-run}/bin/appimage-run";
@@ -89,47 +49,258 @@
       offset = 0;
       mask = ''\xff\xff\xff\xff\x00\x00\x00\x00\xff\xff\xff'';
       magicOrExtension = ''\x7fELF....AI\x02'';
-      };
-    
+    };
+
     plymouth.enable = true;
   };
 
-  # GRUB Bootloader theme. Of course you need to enable GRUB above.. duh! and also, enable it on flake.nix
-  #distro-grub-themes = {
-  #  enable = true;
-  #  theme = "nixos";
-  #};
-
-  # Extra Module Options
   drivers = {
-    amdgpu.enable = true;
-    intel.enable = true;
-    nvidia.enable = false;
-    nvidia-prime = {
-       enable = false;
-         intelBusID = "";
-         nvidiaBusID = "";
-    };
+    amdgpu.enable = false;
+    intel.enable = false;
+    nvidia.enable = true;
+    nvidia-prime.enable = false;
   };
+
   vm.guest-services.enable = false;
   local.hardware-clock.enable = false;
 
-  # networking
   networking = {
     networkmanager.enable = true;
     hostName = "${host}";
     timeServers = options.networking.timeServers.default ++ [ "pool.ntp.org" ];
-  }; 
+  };
 
-  # Set your time zone.
-  services.automatic-timezoned.enable = true; #based on IP location
-  
-  #https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
-  #time.timeZone = "Asia/Seoul"; # Set local timezone
+  services = {
+    xserver = {
+      enable = false;
+      xkb.layout = keyboardLayout;
+    };
 
-  # Select internationalisation properties.
+    greetd = {
+      enable = true;
+      vt = 3;
+      settings.default_session = {
+        user = username;
+        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd Hyprland";
+      };
+    };
+
+    smartd.enable = false;
+    gvfs.enable = true;
+    tumbler.enable = true;
+
+    pipewire = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+      wireplumber.enable = true;
+    };
+
+    udev.enable = true;
+    envfs.enable = true;
+    dbus.enable = true;
+    fstrim.enable = true;
+    fstrim.interval = "weekly";
+
+    libinput.enable = true;
+    rpcbind.enable = false;
+    nfs.server.enable = false;
+    openssh.enable = true;
+    flatpak.enable = true;
+    blueman.enable = true;
+    fwupd.enable = true;
+    upower.enable = true;
+    gnome.gnome-keyring.enable = true;
+  };
+
+  systemd.services.flatpak-repo = {
+    path = [ pkgs.flatpak ];
+    script = ''
+      flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+    '';
+  };
+
+  zramSwap = {
+    enable = true;
+    priority = 100;
+    memoryPercent = 30;
+    swapDevices = 1;
+    algorithm = "zstd";
+  };
+
+  powerManagement = {
+    enable = true;
+    cpuFreqGovernor = "Performance";
+  };
+
+  hardware = {
+    logitech = {
+      wireless.enable = false;
+      wireless.enableGraphical = false;
+    };
+    bluetooth = {
+      enable = true;
+      powerOnBoot = true;
+      settings.General = {
+        Enable = "Source,Sink,Media,Socket";
+        Experimental = true;
+      };
+    };
+    graphics.enable = true;
+  };
+
+  security = {
+    rtkit.enable = true;
+    polkit = {
+      enable = true;
+      extraConfig = ''
+        polkit.addRule(function(action, subject) {
+          if (
+            subject.isInGroup("users") &&
+            (action.id == "org.freedesktop.login1.reboot" ||
+             action.id == "org.freedesktop.login1.power-off")
+          ) {
+            return polkit.Result.YES;
+          }
+        })
+      '';
+    };
+    pam.services.swaylock.text = "auth include login";
+  };
+
+  nix = {
+    settings = {
+      auto-optimise-store = true;
+      experimental-features = [ "nix-command" "flakes" ];
+      substituters = [ "https://hyprland.cachix.org" ];
+      trusted-public-keys = [ "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" ];
+    };
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 7d";
+    };
+  };
+
+  virtualisation.libvirtd.enable = false;
+  virtualisation.podman.enable = false;
+
+  console.keyMap = keyboardLayout;
+
+  environment = {
+    sessionVariables.NIXOS_OZONE_WL = "1";
+    shells = with pkgs; [ zsh ];
+
+	variables = {
+		EDITOR = "micro";
+		SYSTEMD_EDITOR = "micro";
+		VISUAL = "micro";
+	};
+
+    systemPackages = with pkgs; [
+      lsd fzf bc baobab btrfs-progs clang curl cpufrequtils duf findutils ffmpeg
+      glib gsettings-qt git git-lfs killall libappindicator libnotify openssl pciutils wget
+      xdg-user-dirs xdg-utils micro fastfetch
+      (mpv.override { scripts = [ mpvScripts.mpris ]; })
+      inputs.ags.packages.${pkgs.system}.default
+      brightnessctl cava cliphist loupe gnome-system-monitor grim gtk-engine-murrine
+      hypridle imagemagick inxi jq kitty libsForQt5.qtstyleplugin-kvantum networkmanagerapplet
+      nwg-displays nwg-look nvtopPackages.full pamixer pavucontrol playerctl polkit_gnome
+      libsForQt5.qt5ct kdePackages.qt6ct kdePackages.qtwayland kdePackages.qtstyleplugin-kvantum
+      rofi-wayland slurp swappy swaynotificationcenter swww unzip wallust wl-clipboard wlogout
+      xarchiver yad yt-dlp gamemode ncspot mangohud inputs.zen-browser.packages."${system}".default goverlay protonup-qt
+      # Add packages / add programs above here
+      (pkgs.python3.withPackages (ps: with ps; [ requests pyquery ]))
+    ];
+  };
+
+  users = {
+    mutableUsers = true;
+    users."${username}" = {
+      homeMode = "755";
+      isNormalUser = true;
+      description = gitUsername;
+      extraGroups = [ "networkmanager" "wheel" "libvirtd" "scanner" "lp" "video" "input" "audio" ];
+      packages = [ ];
+    };
+    defaultUserShell = pkgs.zsh;
+  };
+
+  fonts.packages = with pkgs; [
+    dejavu_fonts fira-code fira-code-symbols font-awesome hackgen-nf-font ibm-plex inter
+    jetbrains-mono material-icons maple-mono.NF minecraftia nerd-fonts.im-writing
+    nerd-fonts.blex-mono noto-fonts noto-fonts-emoji noto-fonts-cjk-sans noto-fonts-cjk-serif
+    noto-fonts-monochrome-emoji powerline-fonts roboto roboto-mono symbola terminus_font
+    victor-mono
+  ];
+
+  programs = {
+    hyprland = {
+      enable = true;
+      portalPackage = pkgs.xdg-desktop-portal-hyprland;
+      xwayland.enable = true;
+    };
+
+    waybar.enable = true;
+    hyprlock.enable = true;
+    git.enable = true;
+    nm-applet.indicator = true;
+    thunar = {
+      enable = true;
+      plugins = with pkgs.xfce; [
+        exo mousepad thunar-archive-plugin thunar-volman tumbler
+      ];
+    };
+    steam = {
+      enable = true;
+      gamescopeSession.enable = true;
+      remotePlay.openFirewall = true;
+      dedicatedServer.openFirewall = true;
+    };
+    xwayland.enable = true;
+    dconf.enable = true;
+    seahorse.enable = true;
+    fuse.userAllowOther = true;
+    mtr.enable = true;
+    gnupg.agent = {
+      enable = true;
+      enableSSHSupport = true;
+    };
+    zsh = {
+      enable = true;
+      enableCompletion = true;
+      ohMyZsh = {
+        enable = true;
+        plugins = [ "git" ];
+        theme = "agnoster";
+      };
+      autosuggestions.enable = true;
+      syntaxHighlighting.enable = true;
+      promptInit = ''
+        fastfetch -c $HOME/.config/fastfetch/config-compact.jsonc
+        alias ls='lsd'
+        alias l='ls -l'
+        alias la='ls -a'
+        alias lla='ls -la'
+        alias lt='ls --tree'
+        source <(fzf --zsh)
+        HISTFILE=~/.zsh_history
+        HISTSIZE=10000
+        SAVEHIST=10000
+        setopt appendhistory
+      '';
+    };
+  };
+
+  xdg.portal = {
+    enable = true;
+    wlr.enable = false;
+    extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+    configPackages = [ pkgs.xdg-desktop-portal-gtk pkgs.xdg-desktop-portal ];
+  };
+
   i18n.defaultLocale = "en_US.UTF-8";
-
   i18n.extraLocaleSettings = {
     LC_ADDRESS = "en_US.UTF-8";
     LC_IDENTIFICATION = "en_US.UTF-8";
@@ -142,220 +313,8 @@
     LC_TIME = "en_US.UTF-8";
   };
 
+  services.automatic-timezoned.enable = true;
 
-  # Services to start
-  services = {
-    xserver = {
-      enable = false;
-      xkb = {
-        layout = "${keyboardLayout}";
-        variant = "";
-      };
-    };
-    
-    greetd = {
-      enable = true;
-      vt = 3;
-      settings = {
-        default_session = {
-          user = username;
-          command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd Hyprland"; # start Hyprland with a TUI login manager
-        };
-      };
-    };
-    
-    smartd = {
-      enable = false;
-      autodetect = true;
-    };
-    
-	  gvfs.enable = true;
-	  tumbler.enable = true;
-
-	  pipewire = {
-      enable = true;
-      alsa.enable = true;
-      alsa.support32Bit = true;
-      pulse.enable = true;
-	    wireplumber.enable = true;
-  	  };
-	
-    #pulseaudio.enable = false; #unstable
-	  udev.enable = true;
-	  envfs.enable = true;
-	  dbus.enable = true;
-
-	  fstrim = {
-      enable = true;
-      interval = "weekly";
-      };
-  
-    libinput.enable = true;
-
-    rpcbind.enable = false;
-    nfs.server.enable = false;
-  
-    openssh.enable = true;
-    flatpak.enable = false;
-	
-  	blueman.enable = true;
-  	
-  	#hardware.openrgb.enable = true;
-  	#hardware.openrgb.motherboard = "amd";
-
-	  fwupd.enable = true;
-
-	  upower.enable = true;
-    
-    gnome.gnome-keyring.enable = true;
-    
-    #printing = {
-    #  enable = false;
-    #  drivers = [
-        # pkgs.hplipWithPlugin
-    #  ];
-    #};
-    
-    #avahi = {
-    #  enable = true;
-    #  nssmdns4 = true;
-    #  openFirewall = true;
-    #};
-    
-    #ipp-usb.enable = true;
-    
-    #syncthing = {
-    #  enable = false;
-    #  user = "${username}";
-    #  dataDir = "/home/${username}";
-    #  configDir = "/home/${username}/.config/syncthing";
-    #};
-
-  };
-  
-  systemd.services.flatpak-repo = {
-    path = [ pkgs.flatpak ];
-    script = ''
-      flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-    '';
-  };
-
-  # zram
-  zramSwap = {
-	  enable = true;
-	  priority = 100;
-	  memoryPercent = 30;
-	  swapDevices = 1;
-    algorithm = "zstd";
-    };
-
-  powerManagement = {
-  	enable = true;
-	  cpuFreqGovernor = "schedutil";
-  };
-
-  #hardware.sane = {
-  #  enable = true;
-  #  extraBackends = [ pkgs.sane-airscan ];
-  #  disabledDefaultBackends = [ "escl" ];
-  #};
-
-  # Extra Logitech Support
-  hardware = { 
-     logitech.wireless.enable = false;
-     logitech.wireless.enableGraphical = false;
-  }; 
-
-  services.pulseaudio.enable = false; # stable branch
-
-  # Bluetooth
-  hardware = {
-  	bluetooth = {
-	    enable = true;
-	    powerOnBoot = true;
-	    settings = {
-		    General = {
-		      Enable = "Source,Sink,Media,Socket";
-		      Experimental = true;
-		    };
-      };
-    };
-  };
-
-  # Security / Polkit
-  security = { 
-    rtkit.enable = true;
-    polkit.enable = true;
-    polkit.extraConfig = ''
-     polkit.addRule(function(action, subject) {
-       if (
-         subject.isInGroup("users")
-           && (
-             action.id == "org.freedesktop.login1.reboot" ||
-             action.id == "org.freedesktop.login1.reboot-multiple-sessions" ||
-             action.id == "org.freedesktop.login1.power-off" ||
-             action.id == "org.freedesktop.login1.power-off-multiple-sessions"
-           )
-         )
-       {
-         return polkit.Result.YES;
-       }
-    })
-  '';
- };
-  security.pam.services.swaylock = {
-    text = ''
-      auth include login
-    '';
-  };
-
-  # Cachix, Optimization settings and garbage collection automation
-  nix = {
-    settings = {
-      auto-optimise-store = true;
-      experimental-features = [
-        "nix-command"
-        "flakes"
-      ];
-      substituters = [ "https://hyprland.cachix.org" ];
-      trusted-public-keys = [ "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" ];
-    };
-    gc = {
-      automatic = true;
-      dates = "weekly";
-      options = "--delete-older-than 7d";
-    };
-  };
-
-  # Virtualization / Containers
-  virtualisation.libvirtd.enable = false;
-  virtualisation.podman = {
-    enable = false;
-    dockerCompat = false;
-    defaultNetwork.settings.dns_enabled = false;
-  };
-
-  # OpenGL
-  hardware.graphics = {
-    enable = true;
-  };
-
-  console.keyMap = "${keyboardLayout}";
-
-  # For Electron apps to use wayland
-  environment.sessionVariables.NIXOS_OZONE_WL = "1";
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "24.11"; # Did you read the comment?
+  nixpkgs.config.allowUnfree = true;
+  system.stateVersion = "24.11";
 }
